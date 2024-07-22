505d67de157a6219068cca18c35a9d37a7c512e2
Hassan Mahadjir <hm.mahadjir@gmail.com>
2024-07-22 08:03:48 +0300

    examine language translation

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,32 @@
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity } from 'react-native';
+import i18next, { languageResources } from '@/services/i18next';
+import { useTranslation } from 'react-i18next';
+import languageList from '@/services/languagesList.json';
+import { useState } from 'react';
 
-export default function Index() {
+function Index() {
+  const [visible, setVisible] = useState(false);
+  const { t } = useTranslation();
+
+  const changeLng = (lng: string) => {
+    i18next.changeLanguage(lng);
+    setVisible(false);
+  };
+
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>{t('welcome')}</Text>
+      <TouchableOpacity style={{ padding: 10, backgroundColor: 'orange' }}>
+        <Text>{t('change-language')}</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+export default Index;
---
+++ src/constants/language.ts
@@ -0,0 +1,16 @@
+export const LANGUAGE_DATA = [
+  {
+    value: 'tr',
+    lable: 'Türkçe',
+    image: {
+      uri: 'https://flagsapi.com/TR/flat/24.png',
+    },
+  },
+  {
+    value: 'en',
+    lable: 'English',
+    image: {
+      uri: 'https://flagsapi.com/US/flat/24.png',
+    },
+  },
+];
