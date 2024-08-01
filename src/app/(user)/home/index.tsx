a80b71361825659639b9ae56f51c66ed94c8b089
Hassan Mahadjir <hm.mahadjir@gmail.com>
2024-08-01 22:41:28 +0300

    Add user home page and create slider indicator

--- src/app/(user)/home/index.tsx
+++ src/app/(user)/home/index.tsx
@@ -1,10 +1,50 @@
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
+import Header from '@/components/home/Header';
+import TenderNews from '@/components/home/TenderNews';
+import ServiceCategory from '@/components/home/ServiceCategory';
 
 export default function userHomeIndex() {
   return (
-    <View>
-      <Text>userHomeIndex</Text>
+    <View style={{ flex: 1 }}>
+      <Header />
+      <ScrollView style={{ flexGrow: 1 }}>
+        <Text style={style.sectionTitle}>Tender News</Text>
+        <TenderNews />
+
+        <View style={style.categoryWapper}>
+          <Text style={style.sectionTitle}>Service Category</Text>
+          <TouchableOpacity onPress={() => {}}>
+            <Text style={style.seeall}>see all</Text>
+          </TouchableOpacity>
+        </View>
+        <ServiceCategory />
+      </ScrollView>
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  sectionTitle: {
+    color: '#F64D00',
+    marginTop: 3,
+    marginLeft: 10,
+    fontWeight: 'bold',
+  },
+  categoryWapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginRight: 15,
+  },
+  seeall: {
+    fontSize: 16,
+  },
+});
---
+++ src/components/home/TenderNews.tsx
@@ -0,0 +1,147 @@
+import {
+  View,
+  Text,
+  FlatList,
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+import React, { useState } from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
+import tenderNews from '../../assets/data/tenderNews.json';
+
+type SliderItem = {
+  id: number;
+  imageURL: string;
+  description: string;
+};
+
+const terder_News: SliderItem[] = tenderNews;
+
+// Get screen Width
+const screenWidth = Dimensions.get('window').width - 40;
+
+// Render Items to FlatList
+const renderItem = ({ item }: { item: SliderItem }) => {
+  return (
+    <View>
+      <ImageBackground
+        source={{ uri: item.imageURL }}
+        resizeMode="cover"
+        style={style.tenderNewsContainer}
+      >
+        <LinearGradient
+          colors={['rgba(40,53,86,0.9)', 'transparent']}
+          style={style.linearGradientStyle}
+          start={{ x: 0, y: 1 }}
+          end={{ x: 0, y: 0 }}
+        />
+
+        <Text style={style.description}>{item.description}</Text>
+        <TouchableOpacity style={style.readMore}>
+          <Text style={{ color: '#fff' }}>Read More</Text>
+        </TouchableOpacity>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default function TenderNews() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleScroll = (event: {
+    nativeEvent: { contentOffset: { x: number } };
+  }) => {
+    const scrollPosition = event.nativeEvent.contentOffset.x;
+    const index = Math.round(scrollPosition / screenWidth);
+    setActiveIndex(index);
+  };
+
+  // Render Scroll Indicators
+  const renderDotIndicators = (activeIndex: number) => {
+    return terder_News.map((dot, index) => {
+      return (
+        <View
+          key={index}
+          style={[
+            style.dot,
+            activeIndex === index ? style.activeDot : style.inactiveDot,
+          ]}
+        />
+      );
+    });
+  };
+
+  return (
+    <View>
+      <View style={{ marginHorizontal: 10 }}>
+        <FlatList
+          data={terder_News}
+          horizontal={true}
+          bounces={false}
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ gap: 5 }}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={renderItem}
+          onScroll={handleScroll}
+        />
+      </View>
+      <View style={style.dotContainer}>{renderDotIndicators(activeIndex)}</View>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  tenderNewsContainer: {
+    width: 390,
+    height: 180,
+    marginTop: 10,
+  },
+  sectionTitle: {
+    color: '#F64D00',
+    paddingTop: 10,
+  },
+  linearGradientStyle: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    borderRadius: 10,
+  },
+  description: {
+    position: 'absolute',
+    top: 115,
+    marginLeft: 10,
+    fontWeight: 'bold',
+    color: '#fff',
+    width: '80%',
+  },
+  readMore: {
+    position: 'absolute',
+    right: '5%',
+    bottom: '6%',
+  },
+  dotContainer: {
+    flexDirection: 'row',
+    // position: 'absolute',
+    marginTop: 5,
+    alignSelf: 'center',
+  },
+  dot: {
+    marginHorizontal: 2,
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+  },
+  activeDot: {
+    backgroundColor: 'orange',
+    width: 25,
+    height: 10,
+  },
+  inactiveDot: {
+    backgroundColor: 'white',
+  },
+});
