5b9748f102939f471321db871efc6e7b0622761f
Ayesha <21902027@emu.edu.tr>
2024-08-28 21:26:05 +0300

    tourism pages

--- src/app/(user)/home/(tourism)/_layout.tsx
+++ src/app/(user)/home/(tourism)/_layout.tsx
@@ -1,17 +1,22 @@
-import { Stack } from 'expo-router';
-import React from 'react';
-import { COLORS } from '@/constants/Colors';
+import { Stack } from 'expo-router'
+import React from 'react'
+import historicalPlaces from './historicalPlaces';
+import tourismMain from './tourismMain';
+import ghostTown from './ghostTown';
 
 const tourismLayout = () => {
 	return (
-		<Stack
-			screenOptions={{
-				headerStyle: { backgroundColor: COLORS.secondary },
-				headerTintColor: '#fff',
-				headerTitleAlign: 'center',
-			}}
-		>
+		<Stack>
 			<Stack.Screen name='index' options={{ headerShown: false }} />
+			<Stack.Screen
+				name="tourismMain" options={{ headerShown: false}}
+        	/>
+			<Stack.Screen
+				name="historicalPlaces" options={{ headerShown: false}}
+        	/>
+			<Stack.Screen
+				name="ghostTown" options={{ headerShown: false}}
+        	/>
 		</Stack>
 	);
 };
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,5 +2,5 @@ import { Redirect } from 'expo-router';
 import React from 'react';
 
 export default function rootIndex() {
-	return <Redirect href={'./(user)/home/(health)'} />;
+	return <Redirect href={'./(user)/home/(tourism)/tourismMain'} />;
 }
