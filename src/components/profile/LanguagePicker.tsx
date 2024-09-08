be0eb6301d2748a8f70021597d889decc87970c7
Ayesha <21902027@emu.edu.tr>
2024-09-08 14:49:26 +0300

    separated styles for language picker

--- src/components/profile/LanguagePicker.tsx
+++ src/components/profile/LanguagePicker.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, Modal, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
+import { styles } from '@/styles/languagePicker';
 
 interface LanguagePickerProps {
   visible: boolean;
@@ -61,44 +62,5 @@ const LanguagePicker: React.FC<LanguagePickerProps> = ({ visible, onClose }) => 
     </Modal>
   );
 };
-
-const styles = StyleSheet.create({
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'flex-end', // Align at the bottom of the screen
-    alignItems: 'center',
-    marginBottom: '13%'
-  },
-  modalContent: {
-    width: '90%',
-    height: '34%',
-    backgroundColor: 'white',
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    // Add shadow
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: -2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 2, // for Android
-  },
-  headerRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: 15,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-  },
-  item: {
-    padding: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-  },
-  itemText: {
-    fontSize: 18,
-    textAlign: 'center',
-  },
-});
 
 export default LanguagePicker;
---
+++ src/styles/languagePicker.ts
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+	modalContainer: {
+        flex: 1,
+        justifyContent: 'flex-end', // Align at the bottom of the screen
+        alignItems: 'center',
+        marginBottom: '13%'
+      },
+      modalContent: {
+        width: '90%',
+        height: '34%',
+        backgroundColor: 'white',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        // Add shadow
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: -2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 2, // for Android
+      },
+      headerRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 15,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+      },
+      item: {
+        padding: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+      },
+      itemText: {
+        fontSize: 18,
+        textAlign: 'center',
+      },
+});
