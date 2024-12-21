1edde1aa42b3cf3aff88dda24a0073135f971b77
Hassan Mahadjir <hm.mahadjir@gmail.com>
2024-12-21 22:52:54 +0200

    history

--- src/app/(user)/home/(municipality)/history.tsx
+++ src/app/(user)/home/(municipality)/history.tsx
@@ -1,23 +1,30 @@
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Header from '@/components/services/Header';
-import { router, useLocalSearchParams } from 'expo-router';
-import { moderateScale, scale, verticalScale } from 'react-native-size-matters';
-import EvilIcons from '@expo/vector-icons/EvilIcons';
-import { COLORS } from '@/constants/Colors';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { router } from 'expo-router';
 import { useTranslation } from 'react-i18next';
-import { formatDistanceToNow } from 'date-fns';
-import { useGetAnnouncement } from '@/services/api/announcement';
-import Loading from '@/components/Loading';
-import { tr, enUS } from 'date-fns/locale';
-import type { Locale } from 'date-fns';
+import { scale } from 'react-native-size-matters';
 
 const NewsDetails = () => {
 	const { t, i18n } = useTranslation();
 	const lang = i18n.language.toUpperCase();
 
-	return <View>History</View>;
+	const history = '';
+	return (
+		<View>
+			<Header
+				title={t('history')}
+				backgroundImage={{
+					uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDBV0NfJikz6mU0HbEFt6_aywequJEDwp49g&s',
+				}}
+				onBackPress={() => router.back()}
+			/>
+
+			<ScrollView style={{ flexGrow: 1, margin: scale(10) }}>
+				<Text style={{}}>{t('history')}</Text>
+			</ScrollView>
+		</View>
+	);
 };
 
 export default NewsDetails;
